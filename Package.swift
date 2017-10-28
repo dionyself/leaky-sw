68d1509c9d5df6cf2a3c68863e797e4aafdd64d7
Dionys Rosario <dionyself@gmail.com>
2017-10-27 23:31:42 -0400

    graphql

--- Package.swift
+++ Package.swift
@@ -8,12 +8,16 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        //.Package(url: "https://github.com/GraphQLSwift/GraphQL.git", majorVersion: 0),
+        .package(url: "https://github.com/lgaches/Graphiti.git", from: "0.3.2"),
+        .package(url: "https://github.com/IBM-Swift/Kitura.git", from: "1.7.9"),
+        .package(url: "https://github.com/dionyself/Kitura-GraphQL.git", from: "0.0.11"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "leaky",
-            dependencies: []),
+            dependencies: ["Kitura", "Kitura-GraphQL"]),
     ]
 )
--- Sources/leaky/main.swift
+++ Sources/leaky/main.swift
@@ -1,1 +1,27 @@
-print("Hello, world!")
+import Graphiti
+import Kitura
+import GraphQLMiddleware
+
+let schema = try Schema<NoRoot, NoContext> { schema in
+    try schema.query { query in
+        try query.field(name: "hello",
+                        type: String.self,
+                        description: "Totally Awesome",
+                        deprecationReason: nil,
+                        resolve: {
+                            (type, arguments, context, resolveInfo) -> String in
+                            return "world"
+        })
+    }
+}
+
+let router = Router()
+
+let graphQL = GraphQLMiddleware(schema: schema,
+                                showGraphiQL: true,
+                                rootValue: noRootValue)
+
+router.all("/graphql", middleware: graphQL)
+
+Kitura.addHTTPServer(onPort: 8090, with: router)
+Kitura.run()
